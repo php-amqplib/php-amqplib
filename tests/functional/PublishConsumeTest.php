6bfe5bcc17e1b9bfa30b1473b7134f295c12ba93
Alvaro Videla <videlalvaro@gmail.com>
2011-11-17 00:40:45 +0100

    new tests for message properties

--- tests/functional/PublishConsumeTest.php
+++ tests/functional/PublishConsumeTest.php
@@ -23,14 +23,25 @@ class PublishConsumeTest extends PHPUnit_Framework_TestCase
     public function testPublishConsume()
     {
         $this->msg_body = 'foo bar baz äëïöü';
+
         $msg = new AMQPMessage($this->msg_body, array(
             'content_type' => 'text/plain',
-            'delivery-mode' => 1
+            'delivery-mode' => 1,
+            'correlation_id' => 'my_correlation_id',
+            'reply_to' => 'my_reply_to'
         ));
 
         $this->ch->basic_publish($msg, $this->exchange_name, $this->queue_name);
 
-        $this->ch->basic_consume($this->queue_name, getmypid(), false, false, false, false, array($this, 'process_msg'));
+        $this->ch->basic_consume(
+            $this->queue_name,
+            getmypid(),
+            false,
+            false,
+            false,
+            false,
+            array($this, 'process_msg')
+        );
 
         while(count($this->ch->callbacks)) {
             $this->ch->wait();
@@ -45,10 +56,19 @@ class PublishConsumeTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals($this->msg_body, $msg->body);
 
+        //delivery tests
         $this->assertEquals(getmypid(), $delivery_info['consumer_tag']);
         $this->assertEquals($this->queue_name, $delivery_info['routing_key']);
         $this->assertEquals($this->exchange_name, $delivery_info['exchange']);
         $this->assertEquals(false, $delivery_info['redelivered']);
+
+        //msg property tests
+        $this->assertEquals('text/plain', $msg->get('content_type'));
+        $this->assertEquals('my_correlation_id', $msg->get('correlation_id'));
+        $this->assertEquals('my_reply_to', $msg->get('reply_to'));
+
+        $this->setExpectedException('OutOfBoundsException');
+        $msg->get('no_property');
     }
 
     public function tearDown()
