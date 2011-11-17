ba2b973ff7f52af626c4262e8553a0eef2cbac4a
Alvaro Videla <videlalvaro@gmail.com>
2011-11-17 01:36:12 +0100

    file transfer benchmarks

---
+++ benchmark/file_publish.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Usage:
+ *  - Publish 100 5mb messages:
+ *      php file_publish.php 100
+ *  - Publish 1 5mb message:
+ *      php file_publish.php
+ *
+ */
+
+//suboptimal function to generate random content
+function generate_random_content($bytes)
+{
+    $handle = @fopen("/dev/urandom", "rb");
+
+    if ($handle) {
+        $buffer = '';
+        $len = 0;
+        $max = $bytes;
+        while($len < $max-1) {
+            $buffer .= fgets($handle, $max-$len);
+            $len = strlen($buffer);
+        }
+        fclose($handle);
+    }
+
+    return $buffer;
+}
+
+require_once(__DIR__ . '/../amqp.inc');
+include(__DIR__ . '/config.php');
+
+$exchange = 'file_exchange';
+$queue = 'file_queue';
+
+$conn = new AMQPConnection(HOST, PORT, USER, PASS, VHOST);
+$ch = $conn->channel();
+
+$ch->queue_declare($queue, false, false, false, false);
+$ch->exchange_declare($exchange, 'direct', false, false, false);
+$ch->queue_bind($queue, $exchange);
+
+$max = isset($argv[1]) ? (int) $argv[1] : 1;
+$msg_size = 1024*1024*5+1
+$msg = new AMQPMessage(generate_random_content($msg_size));
+
+$time = microtime(true);
+
+// Publishes $max messages using $msg_body as the content.
+for ($i = 0; $i < $max; $i++) {
+    $ch->basic_publish($msg, $exchange);
+}
+
+echo microtime(true) - $time, "\n";
+
+$ch->basic_publish(new AMQPMessage('quit'), $exchange);
+
+$ch->close();
+$conn->close();
+
+?>
