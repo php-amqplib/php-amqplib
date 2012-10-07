2eaea42796f1e917b9b4348a2cbb6a910bf3b023
Alvaro Videla <videlalvaro@gmail.com>
2012-10-07 18:22:06 +0200

    adds basic return example based on issue #33

---
+++ demo/basic_return.php
@@ -0,0 +1,49 @@
+<?php
+use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Connection\AMQPConnection;
+
+require_once('autoload.php');
+
+$connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+// declare  exchange but don`t bind any queue
+$channel->exchange_declare('hidden_exchange', 'topic');
+
+$msg = new AMQPMessage("Hello World!");
+
+echo " [x] Sent non-mandatory ...";
+$channel->basic_publish($msg,
+    'hidden_exchange',
+    'rkey');
+echo " done.\n";
+
+$wait = true;
+
+$return_listener = function ($reply_code, $reply_text,
+    $exchange, $routing_key, $msg) use ($wait){
+    $GLOBALS['wait'] = false;
+
+    echo "return: ",
+    $reply_code, "\n",
+    $reply_text, "\n",
+    $exchange, "\n",
+    $routing_key, "\n",
+    $msg->body, "\n";
+};
+
+$channel->set_return_listener($return_listener);
+
+echo " [x] Sent mandatory ... ";
+$channel->basic_publish($msg,
+    'hidden_exchange',
+    'rkey',
+    true );
+echo " done.\n";
+
+while($wait) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
