5a5c861ac5d9bcde5be1569dcd394285d277cffd
Romain Neutron <imprec@gmail.com>
2012-12-22 21:17:32 +0100

    Remove duplicate code

--- PhpAmqpLib/Exception/AMQPChannelException.php
+++ PhpAmqpLib/Exception/AMQPChannelException.php
@@ -6,8 +6,4 @@ use PhpAmqpLib\Exception\AMQPException;
 
 class AMQPChannelException extends AMQPException
 {
-    public function __construct($reply_code, $reply_text, $method_sig)
-    {
-        parent::__construct($reply_code, $reply_text, $method_sig);
-    }
 }
