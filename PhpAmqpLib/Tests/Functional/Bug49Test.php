f4e9c05d4b51e0b5cec67db4a594b1ef0bc35284
Romain Neutron <imprec@gmail.com>
2013-01-09 10:59:42 +0100

    Fix unit tests

--- PhpAmqpLib/Tests/Functional/Bug49Test.php
+++ PhpAmqpLib/Tests/Functional/Bug49Test.php
@@ -13,17 +13,25 @@ class Bug49Test extends \PHPUnit_Framework_TestCase
     protected $queue_name2 = null;
     protected $q1msgs = 0;
 
+    protected $conn;
+    protected $ch;
+    protected $ch2;
+
+    public function setUp()
+    {
+        $this->conn = new AMQPConnection(HOST, PORT, USER, PASS, VHOST);
+        $this->ch = $this->conn->channel();
+        $this->ch2 = $this->conn->channel();
+    }
+
     public function testDeclaration()
     {
-        $connection = new AMQPConnection(HOST, PORT, USER, PASS, VHOST);
-        $channel = $connection->channel();
-
         try {
-            $channel->queue_declare('pretty.queue', true, true);
+            $this->ch->queue_declare('pretty.queue', true, true);
             $this->fail('Should have raised an exception');
         } catch (AMQPProtocolException $e) {
             if ($e->getCode() == 404) {
-                $channel->queue_declare('pretty.queue', false, true);
+                $this->ch2->queue_declare('pretty.queue', false, true);
             } else {
                 $this->fail('Should have raised a 404 Error');
             }
