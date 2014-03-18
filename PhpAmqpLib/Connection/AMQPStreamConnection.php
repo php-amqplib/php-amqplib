3e42d160280e0f3087f4b3f523e23658983881e7
Eric Berg <ehberg@gmail.com>
2014-03-18 13:27:22 -0400

    Update AMQPStreamConnection.php

--- PhpAmqpLib/Connection/AMQPStreamConnection.php
+++ PhpAmqpLib/Connection/AMQPStreamConnection.php
@@ -20,7 +20,7 @@ class AMQPStreamConnection extends AbstractConnection
     {
         $io = new StreamIO($host, $port, $connection_timeout, $read_write_timeout, $context);
         $this->sock = $io->get_socket();
-
+        
         parent::__construct($user, $password, $vhost, $insist, $login_method, $login_response, $locale, $io);
 
         // save the params for the use of __clone, this will overwrite the parent
