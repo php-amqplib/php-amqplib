799ac6c2463303c116486debd86d9611d1e550f9
Kevin Peek <kevin.peek@pardot.com>
2014-03-31 09:48:38 -0400

    Add error checks and remove the now unnecessary timeout check
    - explicitly check for a read failure and null socket
    - remove the timeout check we added since we shouldn't need it anymore and it makes the code uglier

--- PhpAmqpLib/Wire/IO/SocketIO.php
+++ PhpAmqpLib/Wire/IO/SocketIO.php
@@ -9,14 +9,8 @@ class SocketIO extends AbstractIO
 {
     private $sock = null;
 
-    // Read timeout in secs
-    protected $ioTimeout;
-
-    public function __construct($host, $port, $timeout, $ioTimeout = 90)
+    public function __construct($host, $port, $timeout)
     {
-
-        $this->ioTimeout = $ioTimeout;
-
         $this->sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
         socket_set_option($this->sock, SOL_SOCKET, SO_RCVTIMEO, array('sec' => $timeout, 'usec' => 0));
@@ -37,21 +31,15 @@ class SocketIO extends AbstractIO
         $res = '';
         $read = 0;
 
-        // ensure we don't get stuck in an infinite loop
-        $startTime = time();
-
         $buf = socket_read($this->sock, $n);
-        while ($read < $n && $buf !== '') {
+        while ($read < $n && $buf !== '' && $buf !== false) {
+            // Null sockets are invalid, throw exception
+            if (is_null($this->sock)) {
+                throw new AMQPRuntimeException("Socket was null! Last SocketError was: ".socket_strerror(socket_last_error()));
+            }
             $read += strlen($buf);
             $res .= $buf;
             $buf = socket_read($this->sock, $n - $read);
-
-            // check if ioTimeout has been exceded
-            if ($read < $n && time() - $startTime > $this->ioTimeout) {
-                throw new AMQPIOException("Error reading data. Received " .
-                    strlen($res) . " instead of expected $n bytes - Timed out after ".$this->ioTimeout." secs of looping");
-           }
-
         }
 
         if (strlen($res)!=$n) {
@@ -65,9 +53,6 @@ class SocketIO extends AbstractIO
     public function write($data)
     {
         $len = strlen($data);
-
-        // ensure we don't get stuck in an infinite loop
-        $startTime = time();
 
         while (true) {
             // Null sockets are invalid, throw exception
@@ -87,12 +72,6 @@ class SocketIO extends AbstractIO
                 $data = substr($data, $sent);
                 // Get the length of the not sent part
                 $len -= $sent;
-
-                // check if ioTimeout has been exceded
-                if (time() - $startTime > $this->ioTimeout) {
-                    throw new AMQPIOException("Error writing data. Sent " .
-                        $len . " instead of expected ".strlen($data)." bytes - Timed out after ".$this->ioTimeout." secs of looping");
-                }
             } else {
                 break;
             }
