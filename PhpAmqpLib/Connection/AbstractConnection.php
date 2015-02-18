0347a6b82adb2c609ec8854278dd3c9ccbe36bc1
root <root@localhost.localdomain>
2015-02-18 14:31:28 +0000

    Revert "changed the way prepare content splits the body. now should be a bit more memory efficient"
    This reverts commit 22efa81eeee126d91fa88666f4a1fc3db52bb4fe.

--- PhpAmqpLib/Connection/AbstractConnection.php
+++ PhpAmqpLib/Connection/AbstractConnection.php
@@ -419,16 +419,14 @@ class AbstractConnection extends AbstractChannel
 
         $pkt->write_octet(0xCE);
 
-        // memory efficiency: walk the string instead of biting it. good for very large packets (100+mb)
-        $position = 0;
-        $bodyLength = mb_strlen($body,'8bit');
-        while ($position <= $bodyLength) {
-            $payload = mb_substr($body, $position, $this->frame_max - 8, '8bit');
-            $position += $this->frame_max - 8;
+        while ($body !== '') {
+            $bodyStart = ($this->frame_max - 8);
+            $payload = mb_substr($body, 0, $bodyStart, 'ASCII');
+            $body = (string)mb_substr($body, $bodyStart, mb_strlen($body, 'ASCII') - $bodyStart, 'ASCII');
 
             $pkt->write_octet(3);
             $pkt->write_short($channel);
-            $pkt->write_long(mb_strlen($payload, '8bit'));
+            $pkt->write_long(mb_strlen($payload, 'ASCII'));
 
             $pkt->write($payload);
 
