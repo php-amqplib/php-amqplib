e293ff015cccf25b3e900457c22f2e363ce63970
Todoshchenko Andrey <todoshcenko@tutu.ru>
2015-12-23 12:22:49 +0200

    Default value for socket timeout fixed

--- PhpAmqpLib/Connection/AMQPSocketConnection.php
+++ PhpAmqpLib/Connection/AMQPSocketConnection.php
@@ -28,7 +28,7 @@ class AMQPSocketConnection extends AbstractConnection
         $login_method = 'AMQPLAIN',
         $login_response = null,
         $locale = 'en_US',
-        $timeout = 3.0,
+        $timeout = 3,
         $keepalive = false
     ) {
         $io = new SocketIO($host, $port, $timeout, $keepalive);
