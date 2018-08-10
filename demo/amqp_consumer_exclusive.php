b3c2f55b38c1051b2247f528d64e5c8d82682a7f
Łukasz Korczewski <lukasz.korczewski@gmail.com>
2018-08-10 16:55:11 +0200

    Corrected typo and comment alignment in demo/amqp_consumer_exclusive.php

--- demo/amqp_consumer_exclusive.php
+++ demo/amqp_consumer_exclusive.php
@@ -19,18 +19,18 @@ $channel = $connection->channel();
     name: $queue    // should be unique in fanout exchange. Let RabbitMQ create
                     // a queue name for us
     passive: false  // don't check if a queue with the same name exists
-    durable: false // the queue will not survive server restarts
+    durable: false  // the queue will not survive server restarts
     exclusive: true // the queue can not be accessed by other channels
-    auto_delete: true //the queue will be deleted once the channel is closed.
+    auto_delete: true // the queue will be deleted once the channel is closed.
 */
 list($queueName, ,) = $channel->queue_declare($queue, false, false, true, true);
 
 /*
     name: $exchange
     type: direct
-    passive: false // don't check if a exchange with the same name exists
+    passive: false // don't check if an exchange with the same name exists
     durable: false // the exchange will not survive server restarts
-    auto_delete: true //the exchange will be deleted once the channel is closed.
+    auto_delete: true // the exchange will be deleted once the channel is closed.
 */
 
 $channel->exchange_declare($exchange, 'fanout', false, false, true);
