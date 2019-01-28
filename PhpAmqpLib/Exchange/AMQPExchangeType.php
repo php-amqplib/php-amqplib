5e068b39d42948c79e2db7cacf7864c237dd5b5f
mojun <mojun@corp-ci.com>
2019-01-28 10:32:26 +0800

    change class name AMQPExchange to AMQPExchangeType and constant name,  add some supplement for #640

---
+++ PhpAmqpLib/Exchange/AMQPExchangeType.php
@@ -0,0 +1,10 @@
+<?php
+namespace PhpAmqpLib\Exchange;
+
+final class AMQPExchangeType
+{
+    const DIRECT = 'direct';
+    const FANOUT = 'fanout';
+    const TOPIC = 'topic';
+    const HEADERS = 'headers';
+}
