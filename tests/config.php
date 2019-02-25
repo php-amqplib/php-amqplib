992e5b407e166e1b712e9a121222c19bc6803252
Ramūnas Dronga <github@ramuno.lt>
2019-02-25 17:32:56 +0200

    setup docker based development environment

--- tests/config.php
+++ tests/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('HOST', isset($_ENV['TEST_RABBITMQ_HOST']) ? $_ENV['TEST_RABBITMQ_HOST'] : 'localhost');
-define('PORT', isset($_ENV['TEST_RABBITMQ_PORT']) ? (int)$_ENV['TEST_RABBITMQ_PORT'] : 5672);
-define('USER', isset($_ENV['TEST_RABBITMQ_USER']) ? $_ENV['TEST_RABBITMQ_USER'] : 'guest');
-define('PASS', isset($_ENV['TEST_RABBITMQ_PASS']) ? $_ENV['TEST_RABBITMQ_PASS'] : 'guest');
+define('HOST', getenv('TEST_RABBITMQ_HOST') ? getenv('TEST_RABBITMQ_HOST') : 'localhost');
+define('PORT', getenv('TEST_RABBITMQ_PORT') ? getenv('TEST_RABBITMQ_PORT') : 5672);
+define('USER', getenv('TEST_RABBITMQ_USER') ? getenv('TEST_RABBITMQ_USER') : 'guest');
+define('PASS', getenv('TEST_RABBITMQ_PASS') ? getenv('TEST_RABBITMQ_PASS') : 'guest');
 define('VHOST', '/');
-define('AMQP_DEBUG', isset($_ENV['TEST_AMQP_DEBUG']) ? (bool)$_ENV['TEST_AMQP_DEBUG'] : false);
+define('AMQP_DEBUG', getenv('TEST_AMQP_DEBUG') !== false ? (bool)getenv('TEST_AMQP_DEBUG') : false);
