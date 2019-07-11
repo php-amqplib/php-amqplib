684b188f6f7978673e79fd33a807a8a66e78d519
Ramūnas Dronga <github@ramuno.lt>
2019-07-11 18:18:23 +0300

    script to output php information for debug purposes

---
+++ tests/phpinfo.php
@@ -0,0 +1,17 @@
+<?php
+
+echo '# General info', PHP_EOL;
+phpinfo(INFO_GENERAL);
+echo PHP_EOL;
+
+echo '# Extensions', PHP_EOL;
+print_r(array_intersect(get_loaded_extensions(), ['sockets', 'bcmath']));
+echo PHP_EOL;
+
+echo '# Constants', PHP_EOL;
+$constants = get_defined_constants(true);
+$socketConstant = isset($constants['sockets']) ? $constants['sockets'] : array();
+foreach ($socketConstant as $name => $value) {
+    echo sprintf('%-30s', $name), $value, PHP_EOL;
+}
+
