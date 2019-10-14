71f619e64eff0b5d440e4da83145efcf0c83b54f
terrafrost <terrafrost@php.net>
2019-10-13 19:28:11 -0500

    speed up non-bcmath test

--- tests/config.php
+++ tests/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('HOST', getenv('TEST_RABBITMQ_HOST') ? getenv('TEST_RABBITMQ_HOST') : 'localhost');
+define('HOST', getenv('TEST_RABBITMQ_HOST') ? getenv('TEST_RABBITMQ_HOST') : '127.0.0.1');
 define('PORT', getenv('TEST_RABBITMQ_PORT') ? getenv('TEST_RABBITMQ_PORT') : 5672);
 define('USER', getenv('TEST_RABBITMQ_USER') ? getenv('TEST_RABBITMQ_USER') : 'guest');
 define('PASS', getenv('TEST_RABBITMQ_PASS') ? getenv('TEST_RABBITMQ_PASS') : 'guest');
