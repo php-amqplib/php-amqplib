2d297574a5a94a7cef030929fb566ec32d7a8daf
terrafrost <terrafrost@php.net>
2019-10-14 08:25:18 -0500

    test no-bcmath-mode with phpseclib constants instead

--- tests/config.php
+++ tests/config.php
@@ -1,8 +1,11 @@
 <?php
 
-define('HOST', getenv('TEST_RABBITMQ_HOST') ? getenv('TEST_RABBITMQ_HOST') : '127.0.0.1');
+define('HOST', getenv('TEST_RABBITMQ_HOST') ? getenv('TEST_RABBITMQ_HOST') : 'localhost');
 define('PORT', getenv('TEST_RABBITMQ_PORT') ? getenv('TEST_RABBITMQ_PORT') : 5672);
 define('USER', getenv('TEST_RABBITMQ_USER') ? getenv('TEST_RABBITMQ_USER') : 'guest');
 define('PASS', getenv('TEST_RABBITMQ_PASS') ? getenv('TEST_RABBITMQ_PASS') : 'guest');
 define('VHOST', '/');
 define('AMQP_DEBUG', getenv('TEST_AMQP_DEBUG') !== false ? (bool)getenv('TEST_AMQP_DEBUG') : false);
+if (getenv('BCMATH') && getenv('BCMATH') == 'off') {
+    define('MATH_BIGINTEGER_MODE', \phpseclib\Math\BigInteger::MODE_INTERNAL);
+}
