f746eb44df6d8f838173729867dd1d20b0265faa
Ramūnas Dronga <github@ramuno.lt>
2021-02-23 21:43:54 +0200

    replace conditional phpseclib aliases with defined classes, support autoloader class maps

--- PhpAmqpLib/Helper/BigInteger.php
+++ PhpAmqpLib/Helper/BigInteger.php
@@ -1,11 +1,15 @@
 <?php
-
+// phpcs:ignoreFile
 namespace PhpAmqpLib\Helper;
 
 if (class_exists('phpseclib\Math\BigInteger')) {
-    class_alias('phpseclib\Math\BigInteger', 'PhpAmqpLib\Helper\BigInteger');
+    class BigInteger extends \phpseclib\Math\BigInteger
+    {
+    }
 } elseif (class_exists('phpseclib3\Math\BigInteger')) {
-    class_alias('phpseclib3\Math\BigInteger', 'PhpAmqpLib\Helper\BigInteger');
+    class BigInteger extends \phpseclib3\Math\BigInteger
+    {
+    }
 } else {
     throw new \RuntimeException('Cannot find supported phpseclib/phpseclib library');
 }
