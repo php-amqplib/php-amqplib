c9d103822fe830a0aaeed94170faf8074de5ce98
Oleksandr Dombrovskyi <dombrovsky.alex@gmail.com>
2021-02-26 15:41:12 +0100

    Allow to use SSL connection as lazy (#893)
    * added AMQPLazySSLConnection
    
    Allows to have lazy ssl connection
    
    * dropped deprecated method

---
+++ PhpAmqpLib/Connection/AMQPLazySSLConnection.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PhpAmqpLib\Connection;
+
+class AMQPLazySSLConnection extends AMQPSSLConnection
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function channel($channel_id = null)
+    {
+        $this->connect();
+
+        return parent::channel($channel_id);
+    }
+
+    /**
+     * @return null|\PhpAmqpLib\Wire\IO\AbstractIO
+     */
+    public function getIO()
+    {
+        if (empty($this->io)) {
+            $this->connect();
+        }
+
+        return $this->io;
+    }
+
+    /**
+     * Should the connection be attempted during construction?
+     *
+     * @return bool
+     */
+    public function connectOnConstruct()
+    {
+        return false;
+    }
+}
