8fce64cc321b0dee395409bedf2b36493c01a1bf
patrickkusebauch <patrick.kusebauch@gmail.com>
2021-03-06 11:06:38 +0100

    fix

--- PhpAmqpLib/Helper/Protocol/Protocol080.php
+++ PhpAmqpLib/Helper/Protocol/Protocol080.php
@@ -331,7 +331,7 @@ class Protocol080
      * @param bool $nowait
      * @return array
      */
-    public function exchangeDelete($ticket = 1, $exchange, $if_unused = false, $nowait = false)
+    public function exchangeDelete($ticket = 1, $exchange = '', $if_unused = false, $nowait = false)
     {
         $writer = new AMQPWriter();
         $writer->write_short($ticket);
