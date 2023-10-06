9ef894792eddc301da3a719331b04077b261e3d6
Ramūnas Dronga <github@ramuno.lt>
2023-10-06 22:07:57 +0300

    fix: repeat read select if interrupted by signal and timeout not reached

--- tests/Functional/Channel/ChannelTimeoutTest.php
+++ tests/Functional/Channel/ChannelTimeoutTest.php
@@ -4,6 +4,7 @@ namespace PhpAmqpLib\Tests\Functional\Channel;
 
 use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AbstractConnection;
+use PhpAmqpLib\Exception\AMQPTimeoutException;
 use PhpAmqpLib\Helper\MiscHelper;
 use PhpAmqpLib\Wire\IO\AbstractIO;
 use PhpAmqpLib\Wire\IO\StreamIO;
@@ -29,6 +30,8 @@ class ChannelTimeoutTest extends TestCaseCompat
     /** @var AMQPChannel $channel */
     private $channel;
 
+    private $selectResult = 1;
+
     protected function setUpCompat()
     {
         $channel_rpc_timeout = 3.5;
@@ -40,6 +43,12 @@ class ChannelTimeoutTest extends TestCaseCompat
             ->setConstructorArgs(array(HOST, PORT, 3, 3, null, false, 0))
             ->setMethods(array('select'))
             ->getMock();
+        $this->io
+            ->expects(self::atLeastOnce())
+            ->method('select')
+            ->willReturnCallback(function(){
+                return $this->selectResult;
+            });
         $this->connection = $this->getMockBuilder(AbstractConnection::class)
             ->setConstructorArgs(array(USER, PASS, '/', false, 'AMQPLAIN', null, 'en_US', $this->io, 0, 0, $channel_rpc_timeout))
             ->setMethods(array())
@@ -53,23 +62,19 @@ class ChannelTimeoutTest extends TestCaseCompat
      *
      * @dataProvider provide_operations
      * @param string $operation
-     * @param array $args
+     * @param mixed[] $args
      *
      * @covers \PhpAmqpLib\Channel\AMQPChannel::exchange_declare
      * @covers \PhpAmqpLib\Channel\AMQPChannel::queue_declare
      * @covers \PhpAmqpLib\Channel\AMQPChannel::confirm_select
      */
-    public function should_throw_exception_for_basic_operations_when_timeout_exceeded($operation, $args)
+    public function should_throw_exception_for_basic_operations_when_timeout_exceeded(string $operation, array $args)
     {
-        $this->expectException(\PhpAmqpLib\Exception\AMQPTimeoutException::class);
+        $this->expectException(AMQPTimeoutException::class);
         $this->expectExceptionMessage('The connection timed out after 3.5 sec while awaiting incoming data');
 
         // simulate blocking on the I/O level
-        $this->io->expects($this->any())
-            ->method('select')
-            ->with($this->channel_rpc_timeout_seconds, $this->channel_rpc_timeout_microseconds)
-            ->willReturn(0);
-
+        $this->selectResult = 0;
         call_user_func_array(array($this->channel, $operation), $args);
     }
 
@@ -84,13 +89,7 @@ class ChannelTimeoutTest extends TestCaseCompat
 
     protected function tearDownCompat()
     {
-        if ($this->channel) {
-            $this->channel->close();
-        }
         $this->channel = null;
-        if ($this->connection) {
-            $this->connection->close();
-        }
         $this->connection = null;
     }
 }
