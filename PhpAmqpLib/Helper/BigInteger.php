45f2cddaead371b1d1b026899bce8e61baf09ab1
Guillaume Durand <guillaume.durand@6tm.com>
2026-09-24 15:25:02 +0200

    Add support for phpseclib/phpseclib v4

--- PhpAmqpLib/Helper/BigInteger.php
+++ PhpAmqpLib/Helper/BigInteger.php
@@ -10,6 +10,10 @@ if (class_exists('phpseclib\Math\BigInteger')) {
     class BigInteger extends \phpseclib3\Math\BigInteger
     {
     }
+} elseif (class_exists('phpseclib4\Math\BigInteger')) {
+    class BigInteger extends \phpseclib4\Math\BigInteger
+    {
+    }
 } else {
     throw new \RuntimeException('Cannot find supported phpseclib/phpseclib library');
 }
